a3b75a1f8e65841d019c498557b66b469cfa206e
devanshu-patel12 <devanshu.patel@rapidops.co>
2024-05-30 17:58:02 +0530

    rollup module

---
+++ React/rollupdemo/rollup.config.mjs
@@ -0,0 +1,31 @@
+import babel from "@rollup/plugin-babel";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import replace from "@rollup/plugin-replace";
+
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      file: "public/bundle.js",
+      format: "iife",
+    },
+    plugins: [
+      external(),
+      nodeResolve({ extensions: [".js", ".jsx"] }),
+      babel({
+        babelHelpers: "bundled",
+        presets: [["@babel/preset-react", { runtime: "automatic" }]],
+        extensions: [".js", ".jsx"],
+      }),
+      commonjs(),
+      replace({
+        preventAssignment: false,
+        "process.env.NODE_ENV": '"development"',
+      }),
+      terser(),
+    ],
+  },
+];
