73a57174cc0067b3a2871f916eec13ed3d2ca291
devanshu-patel12 <devanshu.patel@rapidops.co>
2024-06-05 18:22:55 +0530

    git practice

--- React/rollupbuilddemo/src/App.js
+++ React/rollupbuilddemo/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 // import add from "build/cjs/RenderAdd";
-import { add } from "calculator";
+import { add, sub } from "calculator";
 // import myFun from "math/math";
 
 function App() {
-  return <div className="App">{add(1, 7)}</div>;
+  return (
+    <>
+      <div className="App">{add(1, 7)}</div>;
+      <div className="App">{sub(1, 7)}</div>
+    </>
+  );
 }
 
 export default App;
